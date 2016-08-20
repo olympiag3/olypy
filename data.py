623578ebe2f21d0baa1373e89b6b7a13bb647c49
Greg Lindahl <lindahl@pbm.com>
2016-08-20 08:58:56 -0700

    split data functions out of modifylib

---
+++ data.py
@@ -0,0 +1,114 @@
+'''
+Code that manipulates the in-memory Olympia database
+'''
+
+from oid import to_oid, to_int, allocate_oid
+
+def data_append(data, box, subbox, value):
+    if not isinstance(value, list):
+        value = [ value ]
+
+    l = data[box].get(subbox, [])
+    [ l.append(v) for v in value ]
+    data[box][subbox] = l
+
+def data_append2(data, box, subbox, key, value):
+    '''
+    append value to the at data[box][subbox], doing what's needed to initialize things
+    '''
+    data[box] = data.get(box, {})
+    data[box][subbox] = data[box].get(subbox, {})
+    existing = data[box][subbox].get(key, [])
+    existing.append(value)
+    data[box][subbox][key] = existing
+
+def data_newbox(data, oid_kind, firstline, oid=None):
+    if oid:
+        oidint = to_int(oid)
+    else:
+        oidint = allocate_oid(data, oid_kind) # e.g. NNNN
+    if oidint in data:
+        raise ValueError( oidint + ' is already in data')
+    data[oidint] = {}
+    data[oidint]['firstline'] = [str(oidint) + ' ' + firstline]
+    return oidint
+
+def place_new_unit(data, who, where):
+    whoint = to_int(str(who))
+    whereint = to_int(str(where))
+    data_append2(data, whoint, 'LI', 'wh', whereint)
+    data_append2(data, whereint, 'LI', 'hl', whoint)
+
+structures = {
+    # in-progress: bm, er, eg ... bm runs 0..4
+    'roundship': {'type': 'ship', 'de': 10, 'er': 50000, 'ca': 25000,},
+    'galley': {'type': 'ship', 'de': 20, 'er': 25000, 'ca': 5000,},
+    'inn': {'de': 10, 'er': 30000,},
+    'mine': {'de': 10, 'er': 50000, 'sd': 3, }, # depth is sd//3
+    'temple': {'de': 10, 'er': 100000, 'te': 750},
+    'tower': {'de': 40, 'er': 200000,},
+    'castle': {'de': 50, 'er': 1000000,},
+    'castle1': {'kind': 'castle', 'de': 55, 'er': 1000000, 'cl': 1,},
+    'castle2': {'kind': 'castle', 'de': 60, 'er': 1000000, 'cl': 2,},
+    'castle3': {'kind': 'castle', 'de': 65, 'er': 1000000, 'cl': 3,},
+    'castle4': {'kind': 'castle', 'de': 70, 'er': 1000000, 'cl': 4,},
+    'castle5': {'kind': 'castle', 'de': 75, 'er': 1000000, 'cl': 5,},
+    'castle6': {'kind': 'castle', 'de': 80, 'er': 1000000, 'cl': 6,},
+}
+
+def add_structure(data, kind, where, name, progress=None, damage=None, defense=None, oid=None):
+    if kind not in structures:
+        raise ValueError
+    whereint = to_int(where)
+    if whereint not in data:
+        raise ValueError('whereint ' + str(whereint) + ' is not in data')
+
+    oidint = data_newbox(data, 'NNNN', structures[kind].get('type', 'loc') + ' ' + structures[kind].get('kind', kind), oid=oid)
+    place_new_unit(data, oidint, whereint)
+    data[oidint]['na'] = [ name ]
+
+    # fully-finished structure
+    if 'ca' in structures[kind]:
+        data_append2(data, oidint, 'SL', 'ca', structures[kind]['ca'])
+    if 'cl' in structures[kind]:
+        data_append2(data, oidint, 'SL', 'cl', structures[kind]['cl'])
+    if 'sd' in structures[kind]:
+        data_append2(data, oidint, 'SL', 'sd', structures[kind]['sd'])
+    data_append2(data, oidint, 'SL', 'de', defense or structures[kind]['de'])
+    if damage:
+        data_append2(data, oidint, 'SL', 'da', damage)
+
+    # XXX if under construction
+    # remove ca if present
+    # remove de
+    #data_append2(data, oidint, 'SL', 'er', structures[kind]['er'])
+    # compute eg
+    # compute bm 0-4
+    if progress:
+        raise ValueError
+
+def add_scroll(data, skill, loc, oid=None):
+    oidint = data_newbox(data, 'CNNN', 'item scroll', oid=oid)
+    locint = to_int(loc)
+
+    data[oidint]['na'] = [ 'Scroll of '+skill ]
+    data[oidint]['IT'] = {}
+    data[oidint]['IT']['wt'] = [ 1 ]
+    data[oidint]['IT']['un'] = [ locint ]
+    data[oidint]['IM'] = {}
+    data[oidint]['IM']['ms'] = [ skill ]
+
+    data_append(data, locint, 'il', [ oidint, 1 ])
+
+def add_potion(data, kind, im, loc, oid=None):
+    oidint = data_newbox(data, 'CNNN', 'item 0', oid=oid)
+    locint = to_int(loc)
+
+    data[oidint]['na'] = [ 'Potion of '+kind ]
+    data[oidint]['IT'] = {}
+    data[oidint]['IT']['wt'] = [ 1 ]
+    data[oidint]['IT']['un'] = [ locint ]
+    data[oidint]['IM'] = im
+
+    data_append(data, locint, 'il', [ oidint, 1 ])
+
--- modifylib.py
+++ modifylib.py
@@ -5,106 +5,13 @@ Read an entire lib directory
 import argparse
 
 from oid import to_oid, to_int, allocate_oid
-from oio import read_players, write_players, read_lib, write_lib
+from oio import read_lib, write_lib
+from data import place_new_unit, add_structure, add_scroll, add_potion
 
 parser = argparse.ArgumentParser(description='read an Olympia lib')
 
 parser.add_argument('libdir', help='Olympia lib directory')
 args = parser.parse_args()
-
-def data_append(data, box, subbox, value):
-    if not isinstance(value, list):
-        value = [ value ]
-
-    l = data[box].get(subbox, [])
-    [ l.append(v) for v in value ]
-    data[box][subbox] = l
-
-def data_append2(data, box, subbox, key, value):
-    '''
-    append value to the at data[box][subbox], doing what's needed to initialize things
-    '''
-    data[box] = data.get(box, {})
-    data[box][subbox] = data[box].get(subbox, {})
-    existing = data[box][subbox].get(key, [])
-    existing.append(value)
-    data[box][subbox][key] = existing
-
-def data_newbox(data, oid_kind, firstline, oid=None):
-    if oid:
-        oidint = to_int(oid)
-    else:
-        oidint = allocate_oid(data, oid_kind) # e.g. NNNN
-    if oidint in data:
-        raise ValueError( oidint + ' is already in data')
-    data[oidint] = {}
-    data[oidint]['firstline'] = [str(oidint) + ' ' + firstline]
-    return oidint
-
-def place_new_unit(data, oidint, whereint):
-    data_append2(data, oidint, 'LI', 'wh', whereint)
-    data_append2(data, whereint, 'LI', 'hl', oidint)
-
-structures = {
-    # in-progress: bm, er, eg ... bm runs 0..4
-    'roundship': {'type': 'ship', 'de': 10, 'er': 50000, 'ca': 25000,},
-    'galley': {'type': 'ship', 'de': 20, 'er': 25000, 'ca': 5000,},
-    'inn': {'de': 10, 'er': 30000,},
-    'mine': {'de': 10, 'er': 50000, 'sd': 3, }, # depth is sd//3
-    'temple': {'de': 10, 'er': 100000, 'te': 750},
-    'tower': {'de': 40, 'er': 200000,},
-    'castle': {'de': 50, 'er': 1000000,},
-    'castle1': {'kind': 'castle', 'de': 55, 'er': 1000000, 'cl': 1,},
-    'castle2': {'kind': 'castle', 'de': 60, 'er': 1000000, 'cl': 2,},
-    'castle3': {'kind': 'castle', 'de': 65, 'er': 1000000, 'cl': 3,},
-    'castle4': {'kind': 'castle', 'de': 70, 'er': 1000000, 'cl': 4,},
-    'castle5': {'kind': 'castle', 'de': 75, 'er': 1000000, 'cl': 5,},
-    'castle6': {'kind': 'castle', 'de': 80, 'er': 1000000, 'cl': 6,},
-}
-
-def add_structure(data, kind, where, name, progress=None, damage=None, defense=None, oid=None):
-    if kind not in structures:
-        raise ValueError
-    whereint = to_int(where)
-    if whereint not in data:
-        raise ValueError('whereint ' + str(whereint) + ' is not in data')
-
-    oidint = data_newbox(data, 'NNNN', structures[kind].get('type', 'loc') + ' ' + structures[kind].get('kind', kind), oid=oid)
-    place_new_unit(data, oidint, whereint)
-    data[oidint]['na'] = [ name ]
-
-    # fully-finished structure
-    if 'ca' in structures[kind]:
-        data_append2(data, oidint, 'SL', 'ca', structures[kind]['ca'])
-    if 'cl' in structures[kind]:
-        data_append2(data, oidint, 'SL', 'cl', structures[kind]['cl'])
-    if 'sd' in structures[kind]:
-        data_append2(data, oidint, 'SL', 'sd', structures[kind]['sd'])
-    data_append2(data, oidint, 'SL', 'de', defense or structures[kind]['de'])
-    if damage:
-        data_append2(data, oidint, 'SL', 'da', damage)
-
-    # XXX if under construction
-    # remove ca if present
-    # remove de
-    #data_append2(data, oidint, 'SL', 'er', structures[kind]['er'])
-    # compute eg
-    # compute bm 0-4
-    if progress:
-        raise ValueError
-
-def add_scroll(data, skill, loc, oid=None):
-    oidint = data_newbox(data, 'CNNN', 'item scroll', oid=oid)
-    locint = to_int(loc)
-
-    data[oidint]['na'] = [ 'Scroll of '+skill ]
-    data[oidint]['IT'] = {}
-    data[oidint]['IT']['wt'] = [ 1 ]
-    data[oidint]['IT']['un'] = [ locint ]
-    data[oidint]['IM'] = {}
-    data[oidint]['IM']['ms'] = [ skill ]
-
-    data_append(data, locint, 'il', [ oidint, 1 ])
 
 def add_scrolls(data):
     ownerprefix = { 'aa1': [ 'a', 'b', 'c', 'd', 'e', ],
@@ -127,18 +34,6 @@ def add_scrolls(data):
             for skill in skills:
                 oid = prefix + skill
                 add_scroll(data, skill, owner, oid=oid)
-
-def add_potion(data, kind, im, loc, oid=None):
-    oidint = data_newbox(data, 'CNNN', 'item 0', oid=oid)
-    locint = to_int(loc)
-
-    data[oidint]['na'] = [ 'Potion of '+kind ]
-    data[oidint]['IT'] = {}
-    data[oidint]['IT']['wt'] = [ 1 ]
-    data[oidint]['IT']['un'] = [ locint ]
-    data[oidint]['IM'] = im
-
-    data_append(data, locint, 'il', [ oidint, 1 ])
 
 def add_potions(data):
     ownerprefix = { 'aa1': [ 'a', ],
@@ -173,12 +68,6 @@ def add_potions(data):
                 im = {'uk': [5], 'pc': [to_int(pc)]}
                 add_potion(data, 'farcast '+pc, im, owner, oid=oid)
 
-def place_char(data, who, where):
-    whoint = to_int(str(who))
-    whereint = to_int(str(where))
-    data_append2(data, whoint, 'LI', 'wh', whereint)
-    data_append2(data, whereint, 'LI', 'hl', whoint)
-
 data = read_lib(args.libdir)
 
 add_scrolls(data)
@@ -199,16 +88,16 @@ add_structure(data, 'castle6', 'w65', 'Paschen Castle', oid='4001')
 
 # add_garrison(loc, castle, oid=oid)
 
-place_char(data, 1100, 1001)
-place_char(data, 1101, 1100)
-place_char(data, 1102, 'c18')
-place_char(data, 1103, 1005)
-place_char(data, 1104, 1001)
-place_char(data, 1105, 1006)
+place_new_unit(data, 1100, 1001)
+place_new_unit(data, 1101, 1100)
+place_new_unit(data, 1102, 'c18')
+place_new_unit(data, 1103, 1005)
+place_new_unit(data, 1104, 1001)
+place_new_unit(data, 1105, 1006)
 
-place_char(data, 2100, 2001)
-place_char(data, 3100, 3001)
-place_char(data, 4100, 4001)
+place_new_unit(data, 2100, 2001)
+place_new_unit(data, 3100, 3001)
+place_new_unit(data, 4100, 4001)
 
 # fix mage aura, add 1100 auraculum
 # copy chars to other factions
