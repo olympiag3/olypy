c35aad600a4af024b20ef2ece7c16d2a6024607a
Greg Lindahl <lindahl@pbm.com>
2016-09-09 19:56:21 -0700

    shinier and shinier

--- data.py
+++ data.py
@@ -20,7 +20,7 @@ def _uniq_f11(seq):
 
 def data_append(data, box, subbox, value):
     '''
-    append value to data[box][subbox], doing what's needed to initialize things
+    append value to list data[box][subbox], doing what's needed to initialize things
     '''
     box = str(box)
     subbox = str(subbox)
@@ -30,25 +30,25 @@ def data_append(data, box, subbox, value):
     data[box] = data.get(box, {})
     l = data[box].get(subbox, [])
     [ l.append(str(v)) for v in value ]
-    l = uniq_f11(l)
+    l = uniq_f11(l) # XXXv0 replace with if value not in l ...
     data[box][subbox] = l
 
 def data_remove(data, box, subbox, value):
     '''
-    remove value to data[box][subbox]
+    remove value to list data[box][subbox]
     '''
     box = str(box)
     data[box] = data.get(box, {})
     l = data[box].get(subbox, [])
     try:
         l.remove(value)
-        data[box][subbox] = l # only runs if value was in the list
+        data[box][subbox] = l
     except ValueError:
         pass
 
 def data_append2(data, box, subbox, key, value):
     '''
-    append value to data[box][subbox][key], doing what's needed to initialize things
+    append value to list data[box][subbox][key], doing what's needed to initialize things
     '''
     box = str(box)
     subbox = str(subbox)
@@ -59,13 +59,13 @@ def data_append2(data, box, subbox, key, value):
     data[box] = data.get(box, {})
     data[box][subbox] = data[box].get(subbox, {})
     l = data[box][subbox].get(key, [])
-    [ l.append(str(v)) for v in value ] # XXX changeme to an extend
-    l = uniq_f11(l)
+    [ l.append(str(v)) for v in value ]
+    l = uniq_f11(l) # XXXv0 replace with if value not in l ...
     data[box][subbox][key] = l
 
 def data_remove2(data, box, subbox, key, value):
     '''
-    remove value from data[box][subbox][key]
+    remove value from list data[box][subbox][key]
     '''
     box = str(box)
     subbox = str(subbox)
@@ -73,46 +73,141 @@ def data_remove2(data, box, subbox, key, value):
     l = data[box].get(subbox,{}).get(key, [])
     try:
         l.remove(value)
-        data[box][subbox][key] = l # only runs if value was in the list
+        data[box][subbox][key] = l
     except ValueError:
         pass
 
-def upsert_box(data, newbox, promote_children=True):
+def is_char(data, who):
+    if ' char ' in data[who]['firstline'][0]:
+        return True
+
+def can_move(data, who):
     '''
-    As we parse turns going forward in time, we frequently run into boxes
-    that we've seen before. Some are locations, which never move, and
-    others are transient (buildings, characters.) We may have to destroy
-    obsolete information.
+    Used for situations such as deciding to destroy or unlink a no-longer-present box
+    '''
+    if ' loc ' not in data[who]['firstline'][0]:
+        return True
+
+def loop_here(data, who, fogonly=False):
+    '''
+    Make a list of everything here: chars, structures, sublocs
+    If fogonly, make a list of only the invisible things (chars)
+    (Name similar to C code)
+    '''
+    hls = set()
+    if 'LI' in data[who]:
+        if 'hl' in data[who]['LI']:
+            for w in data[who]['LI']['hl']:
+                if fogonly and not is_char(data, w):
+                    continue
+                hls.add(w)
+                [hls.add(x) for x in loop_here(data,w)] # don't propagate fogonly, it only applies to top
+    return hls
+
+def upsert_box(data, newdata, who):
+    '''
+    '''
+
+def upsert_location(data, newdata, top, promote_children=True):
+    '''
+    As we parse turns going forward in time, players observe locations.
+    These might have been seen before, or may have changed. They may
+    contain numerous structures, nobles, etc.
     '''
 
     # if not the same type, destroy the previous thing
-    oidint = newbox.keys()[0]
-    old_firstline = box[oidint]['firstline'] # required
+    old_firstline = data.get(top, {}).get('firstline','99999 nothing nothing')
     _, old_kind, old_subkind = old_firstline.split(' ', maxsplit=2)
-    new_firstline = newbox[oidint]['firstline'] # required
+    new_firstline = newdata[top]['firstline']
     _, new_kind, new_subkind = new_firstline.split(' ', maxsplit=2)
+    # XXXv0 what about foo-in-progress to foo? lot of churn for castle with towers
     if old_kind != new_kind:
-        destroy_box(data, oidint, promote_children=promote_children)
+        if top in data: # likely it is not
+            raise ValueError('hey check this out')
+            destroy_box(data, top, promote_children=promote_children)
+        else:
+            data[top] = {}
 
+    data[top]['firstline'] = newdata[top]['firstline']
+    data[top]['na'] = newdata[top]['na']
 
-    # copy over the name
-    # destroy any interior structures that disappeared
-    #  put existing structures in correct order
-    # unlink any interior nobles that disappeared
-    #  put remaining nobles in correct order
-    # add any interior locations
-    # add interior nobles
-    # market report
-    # do not be confused by
-    #  hidden sub-locs disappearing/appearing; also fog
-    #  tradegoods; opium less visible
-    #  mid-turn trade vs. end-of-turn trade
+    oldhl = loop_here(data[top])
+    newhl = loop_here(newdata[top])
+    gone_or_new = oldhl.symmetric_difference(newhl)
+    gone = gone_or_new.intersect(oldhl)
+    new = gone_or_new.intersect(newhl)
+    invisible_friends = set()
+    if newdata[top].get('foggy'):
+        invisible_friends = loop_here(data[top], fogonly=True)
+        for i in list(invisible_friends):
+            try:
+                gone.remove(i)
+            except KeyError:
+                # this char is visible in newdata. remove from invisible.
+                invisible_friends.remove(i)
+    for g in list(gone):
+        if data[g].get('LO',{}).get('hi'):
+            # hidden sublocs never actually go away
+            gone.remove(g)
+    for g in gone:
+        if can_move(data, g):
+            unset_where(data, g)
+        else:
+            destroy_box(data, g)
+    for n in new:
+        upsert_box(data, newdata, n)
 
-def destroy_box(data, oidint, promote_children=True):
+    # get current things to the right places - can just overwrite wh hl
+    # thanks to the above processing of gone+new
+    for lh in loop_here(newdata, top):
+        hl = newdata[lh].get('LI', {}).get('hl', [])
+        data_overwrite2(data, lh, 'LI', 'hl', hl)
+        for hll in hl:
+            data_overwrite(data, hll, 'LI', 'wh', lh)
+    for f in invisible_friends:
+        # put these on the end, that's OK
+        data_append2(data, lh, 'LI', 'hl', f)
+
+    for lh in loop_here(newdata, top):
+        tl = newdata[lh].get('tl')
+        if tl is not None:
+            data_overwrite(data, lh, 'tl', tl)
+    # mid-turn trade info
+    #  XXXv1 don't trust any city *counts* but end-of-turn; city *prices* do not change
+    #  XXXv1 do figure out if tradegoods have expired: 2 visible mid-turn means others have expired
+
+def dead_char_body(data, who):
+    '''
+    Characters die mid-turn and become dead bodies. The previous
+    turn end-state isn't quite right to freeze, but we'll do that for v0 XXXv1
+    Location is province, or nowhere if at sea (province will be wrong if the char moved and died)
+    '''
+
+    # XXXv1 melters, npcs don't get a body
+    # XXXv0 set a location
+
+    data_overwrite(data, who, 'firstline', str(who) + ' item dead body')
+    data_overwrite2(data, who, 'MI', 'sn', data[who]['na'])
+    data_overwrite(data, who, 'na', 'dead body')
+    pl = data[who]['CH']['lo']
+    data_overwrite2(data, who, 'MI', 'ol', pl)
+    data_overwrite2(data, who, 'IT', 'wt', 100)
+    data_overwrite2(data, who, 'IT', 'pl', 'dead bodies')
+
+    # changing the firstline kind from char has consequences for the player thing
+    # XXXv0 is this complete?
+    # XXXv0 should I just let this get taken care of another way?
+    data_remove2(data, who, 'PL', 'un', who)
+    data_remove2(data, who, 'PL', 'kn', who)
+
+def upsert_char(data, newdata, who):
+    pass
+
+def destroy_box(data, who, promote_children=True):
     '''
     Destroy a box that's become something different.
     '''
-    unlink_box(data, oidint, promote_children=promote_children)
+    unset_where(data, who, promote_children=promote_children)
     # XXXv0 other links:
     # pledge chain - CM,pl is one-way so it needs a end-of-run fixup XXXv0
     # lord: CH,lo and previous lord CH,pl -- needs end-of-run fixup XXXv0
@@ -121,56 +216,63 @@ def destroy_box(data, oidint, promote_children=True):
     # owner of storm - MI,sb {summoned by}
     # storm bound to a ship - ship has SL,bs ... and the storm has MI,bs=itself (?)
 
-def link_box(data, who, where):
-    whoint = to_int(str(who))
-    unlink_box(data, whoint)
-    whereint = to_int(str(where))
-    data_append2(data, whoint, 'LI', 'wh', whereint)
-    data_append2(data, whereint, 'LI', 'hl', whoint)
+def set_where(data, who, where):
+    who = to_int(who)
+    unset_where(data, who)
+    where = to_int(where)
+    data_append2(data, who, 'LI', 'wh', where)
+    existing_hl = data[where].get('LI', {}).get('hl', [])
+    if who not in existing_hl:
+        data_append2(data, where, 'LI', 'hl', who)
 
-def unlink_box(data, oidint, promote_children=True):
+def unset_where(data, who, promote_children=True):
     '''
     If I'm somewhere, remove me from that somewhere.
-    If anyone is inside me, move them up.
+    If promote_children & anyone is inside me, move them up.
     I may well end up nowhere... that will get cleaned up in the end.
+    (Things that move can be nowhere... things that can't move should be destroyed XXXv0)
     '''
-    if data.get(oidint) is None:
+    if data.get(who) is None:
         return
-    wh = data[oidint].get('LI', {}).get('wh')
-    hl = data[oidint].get('LI', {}).get('hl')
+    wh = data[who].get('LI', {}).get('wh', [])
+    hl = data[who].get('LI', {}).get('hl')
 
-    if wh is not None:
-        del data[oidint]['LI']['wh']
+    if len(wh):
+        data[who]['LI']['wh'] = [] # XXXv0 data_overwrite2(...)
         other_hl = data.get(wh[0], {}).get('LI', {}).get('hl')
         if other_hl is not None:
             try:
-                other_hl.remove(oidint)
+                other_hl.remove(who)
                 data[wh[0]]['LI']['hl'] = other_hl
             except ValueError:
                 pass
 
     if promote_children and hl is not None:
-        for h in hl:
-            data_append(data, 'LI', 'hl', wh)
-            data[h]['LI']['wh'] = wh
+        for child in hl:
+            data_append2(data, wh[0], 'LI', 'hl', child)
+            data[child]['LI']['wh'] = wh
 
-def data_newbox(data, oid_kind, firstline, oid=None, overwrite=False):
+# XXXv0 can't have an endless loop of unlink->destroy->unlink 
+#    if not can_move(data, who):
+#        destroy_box(data, who)
+
+def data_newbox(data, oid_kind, firstline, who=None, overwrite=False):
     '''
     Create a new box. Intended for generating QA libs, not for parsing turns.
     '''
-    if oid:
-        oidint = to_int(str(oid))
+    if who:
+        who = to_int(who) # roundtrips if already an int
     else:
-        oidint = allocate_oid(data, oid_kind) # e.g. NNNN
-    if oidint in data:
+        who = allocate_oid(data, oid_kind) # e.g. NNNN
+    if who in data:
         if not overwrite:
-            raise ValueError( oidint + ' is already in data')
+            raise ValueError( who + ' is already in data')
         # if I am not the same thing, destory the old thing
-        if data[oidint]['firstline'] != firstline:
-            destroy_box(data, oidint)
-    data[oidint] = {}
-    data[oidint]['firstline'] = [str(oidint) + ' ' + firstline]
-    return oidint
+        if data[who]['firstline'] != firstline:
+            destroy_box(data, who)
+    data[who] = {}
+    data[who]['firstline'] = [str(who) + ' ' + firstline]
+    return who
 
 structures = {
     # in-progress: bm, er, eg ... bm runs 0..4
@@ -189,60 +291,60 @@ structures = {
     'castle6': {'kind': 'castle', 'de': 80, 'er': 1000000, 'cl': 6,},
 }
 
-def add_structure(data, kind, where, name, progress=None, damage=None, defense=None, oid=None):
+def add_structure(data, kind, where, name, progress=None, damage=None, defense=None, who=None):
     if kind not in structures:
         raise ValueError
-    whereint = to_int(where)
-    if whereint not in data:
-        raise ValueError('whereint ' + str(whereint) + ' is not in data')
+    where = to_int(where)
+    if where not in data:
+        raise ValueError('where ' + where + ' is not in data')
 
-    oidint = data_newbox(data, 'NNNN', structures[kind].get('type', 'loc') + ' ' + structures[kind].get('kind', kind), oid=oid)
-    link_box(data, oidint, whereint)
-    data[oidint]['na'] = [ name ]
+    who = data_newbox(data, 'NNNN', structures[kind].get('type', 'loc') + ' ' + structures[kind].get('kind', kind), who=who)
+    set_where(data, who, where)
+    data[who]['na'] = [ name ]
 
     # fully-finished structure
     if 'ca' in structures[kind]:
-        data_append2(data, oidint, 'SL', 'ca', structures[kind]['ca'])
+        data_append2(data, who, 'SL', 'ca', structures[kind]['ca'])
     if 'cl' in structures[kind]:
-        data_append2(data, oidint, 'SL', 'cl', structures[kind]['cl'])
+        data_append2(data, who, 'SL', 'cl', structures[kind]['cl'])
     if 'sd' in structures[kind]:
-        data_append2(data, oidint, 'SL', 'sd', structures[kind]['sd'])
-    data_append2(data, oidint, 'SL', 'de', defense or structures[kind]['de'])
+        data_append2(data, who, 'SL', 'sd', structures[kind]['sd'])
+    data_append2(data, who, 'SL', 'de', defense or structures[kind]['de'])
     if damage:
-        data_append2(data, oidint, 'SL', 'da', damage)
+        data_append2(data, who, 'SL', 'da', damage)
 
     # XXX if under construction
     # remove ca if present
     # remove de
-    #data_append2(data, oidint, 'SL', 'er', structures[kind]['er'])
+    #data_append2(data, who, 'SL', 'er', structures[kind]['er'])
     # compute eg
     # compute bm 0-4
     if progress:
         raise ValueError
 
-def add_scroll(data, skill, loc, oid=None):
-    oidint = data_newbox(data, 'CNNN', 'item scroll', oid=oid)
-    locint = to_int(loc)
+def add_scroll(data, skill, loc, who=None):
+    who = data_newbox(data, 'CNNN', 'item scroll', who=who)
+    loc = to_int(loc)
     skill = str(skill)
 
-    data[oidint]['na'] = [ 'Scroll of '+skill ]
-    data[oidint]['IT'] = {}
-    data[oidint]['IT']['wt'] = [ 1 ]
-    data[oidint]['IT']['un'] = [ locint ]
-    data[oidint]['IM'] = {}
-    data[oidint]['IM']['ms'] = [ skill ]
+    data[who]['na'] = [ 'Scroll of '+skill ]
+    data[who]['IT'] = {}
+    data[who]['IT']['wt'] = [ 1 ]
+    data[who]['IT']['un'] = [ loc ]
+    data[who]['IM'] = {}
+    data[who]['IM']['ms'] = [ skill ]
 
-    data_append(data, locint, 'il', [ oidint, 1 ])
+    data_append(data, loc, 'il', [ who, 1 ])
 
-def add_potion(data, kind, im, loc, oid=None):
-    oidint = data_newbox(data, 'CNNN', 'item 0', oid=oid)
-    locint = to_int(loc)
+def add_potion(data, kind, im, loc, who=None):
+    who = data_newbox(data, 'CNNN', 'item 0', who=who)
+    loc = to_int(loc)
 
-    data[oidint]['na'] = [ 'Potion of '+kind ]
-    data[oidint]['IT'] = {}
-    data[oidint]['IT']['wt'] = [ 1 ]
-    data[oidint]['IT']['un'] = [ locint ]
-    data[oidint]['IM'] = im
+    data[who]['na'] = [ 'Potion of '+kind ]
+    data[who]['IT'] = {}
+    data[who]['IT']['wt'] = [ 1 ]
+    data[who]['IT']['un'] = [ loc ]
+    data[who]['IM'] = im
 
-    data_append(data, locint, 'il', [ oidint, 1 ])
+    data_append(data, loc, 'il', [ who, 1 ])
 
--- test_data.py
+++ test_data.py
@@ -1,9 +1,15 @@
 import pytest
 
 from oid import to_int, to_oid
+from data import uniq_f11
 from data import data_append, data_remove, data_append2, data_remove2
-from data import upsert_box, destroy_box
-from data import link_box, add_structure, add_scroll, add_potion
+from data import is_char, can_move, loop_here
+from data import upsert_box, upsert_location
+from data import dead_char_body, upsert_char
+from data import destroy_box
+from data import set_where, unset_where
+from data import data_newbox
+from data import add_structure, add_scroll, add_potion
 
 def check_where(data, unit, loc):
     unitint = to_int(str(unit))
@@ -12,6 +18,10 @@ def check_where(data, unit, loc):
 
     # this will throw ValueError if it fails
     assert data[locint]['LI']['hl'].index(unitint) >= 0
+
+def test_uniq_f11():
+    seq = [1, 2, 3, 2]
+    assert uniq_f11(seq) == [1, 2, 3]
 
 def test_append_remove():
     data = {}
@@ -104,20 +114,90 @@ def test_append2_remove2():
     assert data == {'1001': {'LI': {'hl': ['bar', 'foo']}}}
     return
 
-def test_upsert_destroy_link_box():
+def test_is_char_and_can_move():
+    data = {'1001': {'firstline': ['1001 char 0']}}
+    assert is_char(data, '1001') 
+    assert can_move(data, '1001') 
+    data = {'1001': {'firstline': ['1001 loc tower']}}
+    assert not is_char(data, '1001') 
+    assert not can_move(data, '1001') 
+    return
+
+def test_loop_here():
+    data = {'1001': {'firstline': ['1001 loc tower'], 'LI': {'hl': ['1002']}},
+            '1002': {'firstline': ['1002 char 0'], 'LI': {'hl': ['1003']}},
+            '1003': {'firstline': ['1003 loc tower']},} # yeah, nonsensical
+    assert loop_here(data, '1001') == {'1002', '1003'}
+    assert loop_here(data, '1001', fogonly=True) == {'1002', '1003'}
+    assert loop_here(data, '1002') == {'1003'}
+    assert loop_here(data, '1002', fogonly=True) == set()
+    return
+
+def test_upsert_box():
+    return
+
+def test_upsert_location():
+    return
+
+def test_upsert_char():
+    return
+
+def test_destroy_box():
+    return
+
+def test_set_where():
+    data = {'1001': {'LI': {'wh': ['9999']}},
+            '1002': {},}
+    set_where(data, '1001', '1002')
+    assert data['1002']['LI']['hl'] == ['1001']
+    assert data['1001']['LI']['wh'] == ['1002']
+    set_where(data, '1001', '1002') # should do nothing
+    assert data['1002']['LI']['hl'] == ['1001']
+    assert data['1001']['LI']['wh'] == ['1002']
+    return
+
+def test_unset_where():
+    data = {'1001': {'LI': {'wh': ['9999']}},
+            '1002': {},}
+    unset_where(data, '1001')
+    assert data['1001']['LI']['wh'] == []
+    set_where(data, '1001', '1002')
+    assert data['1002']['LI']['hl'] == ['1001']
+    assert data['1001']['LI']['wh'] == ['1002']
+    unset_where(data, '1001')
+    assert data['1001']['LI']['wh'] == []
+    assert data['1002']['LI']['hl'] == []
+    set_where(data, '1001', '1002')
+    data['1002']['LI']['hl'] = [] # so I'm not on the list to be removed
+    unset_where(data, '1001')
+    assert data['1001']['LI']['wh'] == []
+    assert data['1002']['LI']['hl'] == []
+
+    data = {'1001': {'LI': {'wh': ['9999'], 'hl': ['1002']}},
+            '1002': {'LI': {'wh': ['1002'], 'hl': ['1002']}},
+            '9999': {'LI': {'hl': ['1001']}},}
+    unset_where(data, '1001', promote_children=True)
+    assert data['1002']['LI']['wh'] == ['9999']
+    assert data['9999']['LI']['hl'] == ['1002']
+    return
+
+def test_data_newbox():
+    '''
+    Eh, tested by the various add_* below
+    '''
     return
 
 def test_adds():
-    data = {}
-    link_box(data, 1001, 1002)
+    data = {'1002': {}, '1004': {}} # we insist that the target location exists
+    set_where(data, 1001, 1002)
     check_where(data, '1001', '1002')
-    link_box(data, '1003', '1004')
+    set_where(data, '1003', '1004')
     check_where(data, 1003, 1004)
-    link_box(data, '1003', '1004') # second time shouldn't change anything
+    set_where(data, '1003', '1004') # second time shouldn't change anything
     check_where(data, 1003, 1004)
 
     data = {'1006': {}}
-    add_structure(data, 'tower', '1006', 'Foo', oid='1005')
+    add_structure(data, 'tower', '1006', 'Foo', who='1005')
     result = {'1005': {'firstline': ['1005 loc tower'],
                        'na': ['Foo'],
                        'LI': {'wh': ['1006']},
@@ -127,7 +207,7 @@ def test_adds():
     assert data == result
 
     data = {'1007': {}}
-    add_scroll(data, 600, 1007, oid=1008)
+    add_scroll(data, 600, 1007, who=1008)
     result = {'1007': {'il': ['1008', '1']},
               '1008': {'IM': {'ms': ['600']},
                        'IT': {'un': ['1007'], 'wt': [1]},
@@ -137,7 +217,7 @@ def test_adds():
     assert data == result
 
     data = {'1009': {}}
-    add_potion(data, 'heal', {'uk': ['2']}, 1009, oid=1010)
+    add_potion(data, 'heal', {'uk': ['2']}, 1009, who=1010)
     result = {'1010': {'IM': {'uk': ['2']},
                        'IT': {'un': ['1009'], 'wt': [1]},
                        'firstline': ['1010 item 0'],
