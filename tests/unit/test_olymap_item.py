30838a991fea53d323f7691eb522efb3ad1a245c
RocketJSC <mark.lindahl@gmail.com>
2018-06-17 01:43:17 +0000

    Added unit tests for item.py for Attack, Defense and Missile

--- tests/unit/test_olymap_item.py
+++ tests/unit/test_olymap_item.py
@@ -38,3 +38,42 @@ def test_get_prominent():
         assert olymap.item.get_prominent(box) == answer
 
 
+def test_get_item_attack():
+    tests = (
+        ({}, None),
+        ({'IT': {'at': ['60']}}, '60'),
+        ({'IT': {'at': ['0']}}, '0'),
+        ({'IT': {'de': ['60']}}, None),
+        ({'IM': {'at': ['60']}}, None),
+    )
+
+    for box, answer in tests:
+        assert olymap.item.get_item_attack(box) == answer
+
+
+def test_get_item_defense():
+    tests = (
+        ({}, None),
+        ({'IT': {'de': ['60']}}, '60'),
+        ({'IT': {'de': ['0']}}, '0'),
+        ({'IT': {'at': ['60']}}, None),
+        ({'IM': {'de': ['60']}}, None),
+    )
+
+    for box, answer in tests:
+        assert olymap.item.get_item_defense(box) == answer
+
+
+def test_get_item_missile():
+    tests = (
+        ({}, None),
+        ({'IT': {'mi': ['60']}}, '60'),
+        ({'IT': {'mi': ['0']}}, '0'),
+        ({'IT': {'de': ['60']}}, None),
+        ({'IM': {'mi': ['60']}}, None),
+    )
+
+    for box, answer in tests:
+        assert olymap.item.get_item_missile(box) == answer
+
+
